95d48109d0673a63077235611c878cf019f9d479
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2023-11-06 15:14:55 +0100

    LAB2 - print mesurements

--- PyCv/main.py
+++ PyCv/main.py
@@ -1,14 +1,32 @@
 import cv2
+import numpy as np
 
-A = cv2.imread('./DIBCO/H12.png')
+# Let's load a simple image with 3 black squares
+image = cv2.imread('./DIBCO/H04.png')
+cv2.waitKey(0)
 
+# Grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# Find Canny edges
+edged = cv2.Canny(gray, 30, 200)
+cv2.waitKey(0)
 
-# HSV_Image = cv2.cvtColor(A, cv2.COLOR_BGR2HLS)
-# HSV_Gray = HSV_Image[:, :, 1]
-#
-# HSV_Niblack = cv2.ximgproc.niBlackThreshold(HSV_Gray, 255, cv2.THRESH_BINARY, 199, -0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_NIBLACK)
-#
-# cv2.imshow("A", HSV_Niblack)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# Finding Contours
+# Use a copy of the image e.g. edged.copy()
+# since findContours alters the image
+contours, hierarchy = cv2.findContours(edged,
+                                       cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+cv2.imshow('Canny Edges After Contouring', edged)
+cv2.waitKey(0)
+
+print("Number of Contours found = " + str(len(contours)))
+
+# Draw all contours
+# -1 signifies drawing all contours
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+cv2.imshow('Contours', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
