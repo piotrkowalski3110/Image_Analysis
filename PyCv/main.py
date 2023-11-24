d94795a55c6af41840ee1afd8086db980c6bb17e
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2023-11-24 09:28:56 +0100

    Zliczanie obiektu + pole

--- PyCv/main.py
+++ PyCv/main.py
@@ -1,26 +1,12 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
+def zapisz_tablice_do_pliku(tablica, nazwa_pliku):
+  with open(nazwa_pliku, "w") as plik:
+    for wiersz in tablica:
+      plik.write(" ".join(str(elem) for elem in wiersz) + "\n")
 
-IMG = cv2.imread("E09.bmp")
 
-kernel = np.ones((3, 3), np.uint8)
+# Przykład użycia
 
-base = IMG.copy()
-[dx, dy, dz] = base.shape
+tablica = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+nazwa_pliku = "tablica.txt"
 
-operator = base.copy()
-operator[1:dx-1, 1:dy-1] = 0
-
-while True:
-    oldOperator = operator.copy()
-    operator = cv2.dilate(operator, kernel, iterations=1)
-    operator = cv2.bitwise_and(operator, base)
-
-    if(np.array_equal(operator, oldOperator)):
-        break
-
-cv2.imshow("Result", operator)
-cv2.imshow("Base", IMG)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+zapisz_tablice_do_pliku(tablica, nazwa_pliku)
